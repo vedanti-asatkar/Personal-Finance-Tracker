98364db45a15883e29304f5aad30bd8d474c9a62
vedanti-asatkar <vedantiasatkar27@gmail.com>
2025-10-31 22:31:32 +0530

    Add files via upload

---
+++ run-gui.bat
@@ -0,0 +1,18 @@
+@echo off
+echo [INFO] Compiling project for GUI...
+
+REM Create a bin directory if it doesn't exist
+if not exist "bin" mkdir bin
+
+REM Compile all .java files into the bin directory
+REM Generate a list of all .java source files
+dir /s /b src\*.java > sources.txt
+
+REM Compile all .java files from the list into the bin directory
+javac -d bin -cp "lib/mysql-connector-j-8.3.0.jar" @sources.txt
+
+REM Clean up the temporary sources file
+del sources.txt
+
+echo [INFO] Starting Personal Finance Manager GUI...
+java -cp "bin;lib/mysql-connector-j-8.3.0.jar" personalfinancemanager.app.FinanceGuiApp
